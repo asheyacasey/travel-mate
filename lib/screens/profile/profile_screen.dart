1b560f69cc5acdb571e7cbda4531caf7404c1886
harbii <vonnehrvy98@gmail.com>
2022-08-09 16:33:42 +0800

    fixed bottom pixel overflow

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -21,118 +21,122 @@ class ProfileScreen extends StatelessWidget {
       appBar: CustomAppBar(
         title: 'PROFILE',
       ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 10,
-          ),
-          Stack(
-            children: [
-              Container(
-                height: MediaQuery.of(context).size.height / 4,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey,
-                      offset: Offset(3, 3),
-                      blurRadius: 3,
-                      spreadRadius: 3,
-                    ),
-                  ],
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: NetworkImage(user.imageUrls[0]),
-                  ),
-                ),
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height / 4,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  gradient: LinearGradient(
-                    colors: [
-                      Color.fromARGB(0, 0, 0, 0),
-                      Color.fromARGB(150, 0, 0, 0),
-                    ],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                  ),
-                ),
-                child: Align(
-                  alignment: Alignment.bottomLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.only(bottom: 10, left: 10),
-                    child: Text(
-                      user.name,
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline1!
-                          .copyWith(color: Colors.white),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            Stack(
               children: [
-                TitleWithIcon(title: 'Biography', icon: Icons.edit),
-                Text(
-                  user.bio,
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyText1!
-                      .copyWith(height: 1.5),
+                Container(
+                  height: MediaQuery.of(context).size.height / 4,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(3, 3),
+                        blurRadius: 3,
+                        spreadRadius: 3,
+                      ),
+                    ],
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(user.imageUrls[0]),
+                    ),
+                  ),
                 ),
-                TitleWithIcon(title: 'Photos', icon: Icons.edit),
-                SizedBox(
-                  height: 125,
-                  child: ListView.builder(
-                      itemCount: 5,
-                      shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.only(right: 5),
-                          child: Container(
-                            height: 125,
-                            width: 100,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              border: Border.all(
-                                color: Theme.of(context).primaryColor,
-                              ),
-                              image: DecorationImage(
-                                fit: BoxFit.cover,
-                                image: NetworkImage(user.imageUrls[index]),
-                              ),
-                            ),
-                          ),
-                        );
-                      }),
-                ),
-                TitleWithIcon(title: 'Interests', icon: Icons.edit),
-                Row(
-                  children: [
-                    CustomTextContainer(text: 'Music'),
-                  ],
-                ),
-                TitleWithIcon(title: 'Location', icon: Icons.edit),
-                Text(
-                  'UJS-R Basak Campus',
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyText1!
-                      .copyWith(height: 1.5),
+                Container(
+                  height: MediaQuery.of(context).size.height / 4,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    gradient: LinearGradient(
+                      colors: [
+                        Color.fromARGB(0, 0, 0, 0),
+                        Color.fromARGB(150, 0, 0, 0),
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 10, left: 10),
+                      child: Text(
+                        user.name,
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline1!
+                            .copyWith(color: Colors.white),
+                      ),
+                    ),
+                  ),
                 ),
               ],
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TitleWithIcon(title: 'Biography', icon: Icons.edit),
+                  Text(
+                    user.bio,
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyText1!
+                        .copyWith(height: 1.5),
+                  ),
+                  TitleWithIcon(title: 'Photos', icon: Icons.edit),
+                  SizedBox(
+                    height: 125,
+                    child: ListView.builder(
+                        itemCount: 5,
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.only(right: 5),
+                            child: Container(
+                              height: 125,
+                              width: 100,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                border: Border.all(
+                                  color: Theme.of(context).primaryColor,
+                                ),
+                                image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: NetworkImage(user.imageUrls[index]),
+                                ),
+                              ),
+                            ),
+                          );
+                        }),
+                  ),
+                  TitleWithIcon(title: 'Interests', icon: Icons.edit),
+                  Row(
+                    children: [
+                      CustomTextContainer(text: 'Music'),
+                      CustomTextContainer(text: 'Politics'),
+                      CustomTextContainer(text: 'Karaoke'),
+                    ],
+                  ),
+                  TitleWithIcon(title: 'Location', icon: Icons.edit),
+                  Text(
+                    'UJS-R Basak Campus',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyText1!
+                        .copyWith(height: 1.5),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
