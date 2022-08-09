2a0ba8b2ccdcea80d4ce95c6520073499eb73ead
harbii <vonnehrvy98@gmail.com>
2022-08-09 15:42:46 +0800

    displayed main profile photo of the user

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:travel_mate/widgets/custom_appbar.dart';
 
+import '../../models/models.dart';
+
 class ProfileScreen extends StatelessWidget {
   static const String routeName = '/profile';
 
@@ -13,12 +15,66 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final User user = User.users[0];
     return Scaffold(
       appBar: CustomAppBar(
         title: 'PROFILE',
       ),
       body: Column(
-        children: [],
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+          Stack(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height / 4,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(3, 3),
+                      blurRadius: 3,
+                      spreadRadius: 3,
+                    ),
+                  ],
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(user.imageUrls[0]),
+                  ),
+                ),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height / 4,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  gradient: LinearGradient(
+                    colors: [
+                      Color.fromARGB(0, 0, 0, 0),
+                      Color.fromARGB(150, 0, 0, 0),
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                ),
+                child: Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 10, left: 10),
+                    child: Text(
+                      user.name,
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline1!
+                          .copyWith(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
