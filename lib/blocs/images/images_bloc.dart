7aee8c0ca2ec30bed5e649c7eec87ca846e98a1c
Khiara Danielle Rubia <khiara.rubia@usjr.edu.ph>
2022-08-10 14:18:20 +0800

    Photo cannot be seen yet in the phone

---
+++ lib/blocs/images/images_bloc.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:travel_mate/repositories/database/database_repository.dart';
+
+part 'images_event.dart';
+part 'images_state.dart';
+
+class ImagesBloc extends Bloc<ImagesEvent, ImagesState> {
+  final DatabaseRepository _databaseRepository;
+  StreamSubscription? _databaseSubscription;
+
+  ImagesBloc({required DatabaseRepository databaseRepository})
+      : _databaseRepository = databaseRepository,
+        super(ImagesLoading());
+
+  Stream<ImagesState> mapEventToState(ImagesEvent event) async* {
+    if (event is LoadImages) {
+      yield* _mapLoadImagesToState();
+    }
+    if (event is UpdateImages) {
+      yield* _mapUpdateImagesToState(event);
+    }
+  }
+
+  Stream<ImagesState> _mapLoadImagesToState() async* {
+    _databaseSubscription?.cancel();
+
+    _databaseRepository.getUser().listen(
+          (user) => add(
+            UpdateImages(user.imageUrls),
+          ),
+        );
+  }
+
+  Stream<ImagesState> _mapUpdateImagesToState(UpdateImages event) async* {
+    yield ImagesLoaded(imageUrls: event.imageUrls);
+  }
+}
