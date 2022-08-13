59fa0126aa94890d54b93fd307b8d433201ab50e
harbii <vonnehrvy98@gmail.com>
2022-08-13 23:24:22 +0800

    added new input fields on demo tab

--- lib/screens/onboarding/onboarding_screens/demo_screen.dart
+++ lib/screens/onboarding/onboarding_screens/demo_screen.dart
@@ -31,122 +31,159 @@ class Demographics extends StatelessWidget {
           return Padding(
             padding:
                 const EdgeInsets.symmetric(horizontal: 30.0, vertical: 50.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                        width: 90,
-                        height: 90,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Theme.of(context).primaryColor,
-                        ),
-                        child: Icon(
-                          UniconsLine.user_check,
-                          size: 40,
-                          color: Colors.white,
-                        )),
-                    SizedBox(height: 20),
-                    Text(
-                      'About you',
-                      style: Theme.of(context)
-                          .primaryTextTheme
-                          .headline2!
-                          .copyWith(color: Colors.black),
-                    ),
-                    Text(
-                      'We\'d like to know more about you',
-                      style: Theme.of(context).textTheme.headline6!.copyWith(
-                          fontWeight: FontWeight.w900, color: Colors.grey),
-                    ),
-                    SizedBox(height: 20),
-                    CustomTextHeader(
-                      text: 'Choose your Gender',
-                    ),
-                    SizedBox(height: 10),
-                    Container(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                          border: Border.all(
-                              color: Theme.of(context).primaryColorLight,
-                              width: 2.0)),
-                      //color: Colors.red,
-                      child: Row(
-                        children: <Widget>[
-                          Expanded(
-                            flex: 5,
-                            child: Container(
-                              decoration: BoxDecoration(),
-                              child: CustomCheckbox(
-                                text: 'MALE',
-                                value: state.user.gender == 'Male',
-                                onChanged: (bool? newValue) {
-                                  context.read<OnboardingBloc>().add(
-                                        UpdateUser(
-                                          user: state.user
-                                              .copyWith(gender: 'Male'),
-                                        ),
-                                      );
-                                },
-                              ),
-                            ),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                          width: 90,
+                          height: 90,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Theme.of(context).primaryColor,
                           ),
-                          Expanded(
-                            flex: 5,
-                            child: Container(
-                              child: CustomCheckbox(
-                                text: 'FEMALE',
-                                value: state.user.gender == 'Female',
-                                onChanged: (bool? newValue) {
-                                  context.read<OnboardingBloc>().add(
-                                        UpdateUser(
-                                          user: state.user
-                                              .copyWith(gender: 'Female'),
-                                        ),
-                                      );
-                                },
-                              ),
-                            ),
-                          )
-                        ],
+                          child: Icon(
+                            UniconsLine.user_check,
+                            size: 40,
+                            color: Colors.white,
+                          )),
+                      SizedBox(height: 20),
+                      Text(
+                        'About you',
+                        style: Theme.of(context)
+                            .primaryTextTheme
+                            .headline2!
+                            .copyWith(color: Colors.black),
                       ),
-                    ),
-                    SizedBox(height: 50),
-                    CustomTextHeader(
-                      text: 'Enter your Age',
-                    ),
-                    SizedBox(height: 15),
-                    CustomTextField(
-                      hint: 'Enter your Age',
-                      onChanged: (value) {
-                        context.read<OnboardingBloc>().add(
-                              UpdateUser(
-                                user: state.user.copyWith(
-                                  age: int.parse(value),
+                      Text(
+                        'We\'d like to know more about you',
+                        style: Theme.of(context).textTheme.headline6!.copyWith(
+                            fontWeight: FontWeight.w900, color: Colors.grey),
+                      ),
+                      SizedBox(height: 20),
+                      CustomTextHeader(
+                        text: 'Choose your Gender',
+                      ),
+                      SizedBox(height: 10),
+                      Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            border: Border.all(
+                                color: Theme.of(context).primaryColorLight,
+                                width: 2.0)),
+                        //color: Colors.red,
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              flex: 5,
+                              child: Container(
+                                decoration: BoxDecoration(),
+                                child: CustomCheckbox(
+                                  text: 'MALE',
+                                  value: state.user.gender == 'Male',
+                                  onChanged: (bool? newValue) {
+                                    context.read<OnboardingBloc>().add(
+                                          UpdateUser(
+                                            user: state.user
+                                                .copyWith(gender: 'Male'),
+                                          ),
+                                        );
+                                  },
                                 ),
                               ),
-                            );
-                      },
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    StepProgressIndicator(
-                      totalSteps: 6,
-                      currentStep: 3,
-                      selectedColor: Theme.of(context).primaryColor,
-                      unselectedColor: Theme.of(context).backgroundColor,
-                    ),
-                    SizedBox(height: 10),
-                    CustomButton(
-                        tabController: tabController, text: 'NEXT STEP')
-                  ],
-                ),
-              ],
+                            ),
+                            Expanded(
+                              flex: 5,
+                              child: Container(
+                                child: CustomCheckbox(
+                                  text: 'FEMALE',
+                                  value: state.user.gender == 'Female',
+                                  onChanged: (bool? newValue) {
+                                    context.read<OnboardingBloc>().add(
+                                          UpdateUser(
+                                            user: state.user
+                                                .copyWith(gender: 'Female'),
+                                          ),
+                                        );
+                                  },
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 50),
+                      CustomTextHeader(
+                        text: 'What\'s your name?',
+                      ),
+                      SizedBox(height: 15),
+                      CustomTextField(
+                        hint: 'Enter your name',
+                        onChanged: (value) {
+                          context.read<OnboardingBloc>().add(
+                                UpdateUser(
+                                  user: state.user.copyWith(
+                                    name: value,
+                                  ),
+                                ),
+                              );
+                        },
+                      ),
+                      SizedBox(height: 50),
+                      CustomTextHeader(
+                        text: 'What\'s your job?',
+                      ),
+                      SizedBox(height: 15),
+                      CustomTextField(
+                        hint: 'Enter job title',
+                        onChanged: (value) {
+                          context.read<OnboardingBloc>().add(
+                                UpdateUser(
+                                  user: state.user.copyWith(
+                                    jobTitle: value,
+                                  ),
+                                ),
+                              );
+                        },
+                      ),
+                      SizedBox(height: 50),
+                      CustomTextHeader(
+                        text: 'Enter your Age',
+                      ),
+                      SizedBox(height: 15),
+                      CustomTextField(
+                        hint: 'Enter your Age',
+                        onChanged: (value) {
+                          context.read<OnboardingBloc>().add(
+                                UpdateUser(
+                                  user: state.user.copyWith(
+                                    age: int.parse(value),
+                                  ),
+                                ),
+                              );
+                        },
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 50),
+                  Column(
+                    children: [
+                      StepProgressIndicator(
+                        totalSteps: 6,
+                        currentStep: 3,
+                        selectedColor: Theme.of(context).primaryColor,
+                        unselectedColor: Theme.of(context).backgroundColor,
+                      ),
+                      SizedBox(height: 10),
+                      CustomButton(
+                          tabController: tabController, text: 'NEXT STEP')
+                    ],
+                  ),
+                ],
+              ),
             ),
           );
         } else {
