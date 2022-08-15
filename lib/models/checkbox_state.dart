f5217c250edc2703c8183985556fcc5cc6e70c1a
harbii <vonnehrvy98@gmail.com>
2022-08-15 13:35:44 +0800

    created checkbox_state model

---
+++ lib/models/checkbox_state.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class CheckBoxState {
+  final String title;
+  bool value;
+
+  CheckBoxState({
+    required this.title,
+    this.value = false,
+  });
+}
