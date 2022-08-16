fa6a6b3bd935672028ff8f1d00d23751d701130d
Khiara Danielle Rubia <khiara.rubia@usjr.edu.ph>
2022-08-16 18:00:31 +0800

    24. Added missing code in category model

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:travel_mate/models/category_filter_model.dart';
 
 class Category extends Equatable {
   final int id;
@@ -9,8 +10,6 @@ class Category extends Equatable {
     required this.name,
   });
 
-
-
   @override
   List<Object?> get props => [id, name];
 
@@ -18,7 +17,13 @@ class Category extends Equatable {
     Category(id: 1, name: 'Hiking'),
     Category(id: 2, name: 'Swimming'),
     Category(id: 3, name: 'Coffee Hoping'),
-
   ];
 
+  static List<CategoryFilter> filters = Category.categories
+      .map((category) => CategoryFilter(
+            id: category.id,
+            category: category,
+            value: false,
+          ))
+      .toList();
 }
