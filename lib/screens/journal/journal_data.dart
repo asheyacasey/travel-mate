2787f54cdb834b09e11f1df7f9523057843c3110
KHIARA DANIELLE T RUBIA <khiara.rubia@usjr.edu.ph>
2023-05-14 10:25:45 +0800

    New journal. More like a note app

---
+++ lib/screens/journal/journal_data.dart
@@ -0,0 +1,21 @@
+class JournalData {
+  /**
+   * overall list of journals
+   */
+
+  /**
+   * getting the journals
+   */
+
+  /**
+   * adding a new journal
+   */
+
+  /**
+   * updating the journal
+   */
+
+  /**
+   * deleteting the journal
+   */
+}
---
+++ lib/screens/journal/mainJournal.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainJournal extends StatefulWidget {
+  const MainJournal({super.key});
+
+  @override
+  State<MainJournal> createState() => _MainJournalState();
+}
+
+class _MainJournalState extends State<MainJournal> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
