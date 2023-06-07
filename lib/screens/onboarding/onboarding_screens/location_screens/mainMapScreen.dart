e8eee5e7e4c4ce09a7389119e293252d6fdd545c
ashe <asheya.yuson@usjr.edu.ph>
2023-06-07 16:48:35 +0800

    location (currently under modification)

--- lib/screens/onboarding/onboarding_screens/location_screens/mainMapScreen.dart
+++ lib/screens/onboarding/onboarding_screens/location_screens/mainMapScreen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:travel_mate/screens/onboarding/onboarding_screens/location_screens/current_location_screen.dart';
 import 'package:travel_mate/screens/onboarding/onboarding_screens/location_screens/simple_google_map.dart';
+import 'package:travel_mate/screens/onboarding/widgets/widgets.dart';
+import 'package:unicons/unicons.dart';
 
 class MainMapScreen extends StatefulWidget {
   const MainMapScreen({super.key});
@@ -21,38 +24,96 @@ class _MainMapScreenState extends State<MainMapScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Your Location"),
-        centerTitle: true,
-      ),
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
+      body: Padding(
+        padding:
+        const EdgeInsets.symmetric(horizontal: 30.0, vertical: 50.0),
+
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (BuildContext context) {
-                      return const SimpleMapLocation();
-                    },
-                  ),
-                );
-              },
-              child: Text("Simple Map Location"),
+            Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0xFFB0DB2D),
+                ),
+                child: Icon(
+                  UniconsLine.location_point,
+                  size: 36,
+                  color: Colors.white,
+                )
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (BuildContext context) {
-                      return const CurrentLocation();
-                    },
-                  ),
-                );
-              },
-              child: Text("Current Location"),
+            SizedBox(height: 10),
+            Text(
+              'Location',
+              style: GoogleFonts.fredokaOne(
+                textStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
+            SizedBox(height: 10),
+            Text(
+              'To help you find potential matches near your location, we require access to your device\'s location information.',
+              style: GoogleFonts.manrope(
+                textStyle: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+
+            SizedBox(height: 10),
+            // ElevatedButton(
+            //   onPressed: () {
+            //     Navigator.of(context).push(
+            //       MaterialPageRoute(
+            //         builder: (BuildContext context) {
+            //           return const SimpleMapLocation();
+            //         },
+            //       ),
+            //     );
+            //   },
+            //   child: Text("Simple Map Location"),
+            // ),
+            Container(
+              width: double.infinity,
+              child: SizedBox(
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (BuildContext context) {
+                          return const CurrentLocation();
+                        },
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    primary: Color(0xFFB0DB2D),
+                  ),
+                  child: Text(
+                    "Get My Current Location",
+                    style: TextStyle(
+                      fontFamily: 'Manrope',
+                      fontWeight: FontWeight.w900,
+                      fontSize: 16, // Adjust the font size as needed
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+
+
           ],
         ),
       ),
