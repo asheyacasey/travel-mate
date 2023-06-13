c2750c3f85df1b47426febfa9c817c76345fda96
KHIARA DANIELLE T RUBIA <khiara.rubia@usjr.edu.ph>
2023-06-13 14:00:50 +0800

    Added slider (with errors)
    Dili ma move'2 ang slider maybe tungod sa pag apil sa current location into the slider

--- lib/screens/onboarding/onboarding_screens/location_screen.dart
+++ lib/screens/onboarding/onboarding_screens/location_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:step_progress_indicator/step_progress_indicator.dart';
 
 import '../widgets/custom_button.dart';
@@ -35,16 +36,87 @@ class LocationScreen extends StatelessWidget {
               },
               child: Text("Current Location"),
             ),
-            StepProgressIndicator(
-              totalSteps: 6,
-              currentStep: 6,
-              selectedColor: Theme.of(context).primaryColor,
-              unselectedColor: Theme.of(context).colorScheme.background,
+            SizedBox(
+              height: 10,
             ),
+            SliderDemo(),
             SizedBox(height: 10),
-            CustomButton(tabController: tabController, text: 'NEXT STEP')
+            Column(
+              children: [
+                StepProgressIndicator(
+                  totalSteps: 6,
+                  currentStep: 1,
+                  selectedColor: Theme.of(context).primaryColor,
+                  unselectedColor: Theme.of(context).backgroundColor,
+                ),
+                SizedBox(height: 10),
+                CustomButton(
+                  tabController: tabController,
+                  text: 'NEXT STEP',
+                )
+              ],
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class SliderDemo extends StatefulWidget {
+  const SliderDemo({super.key});
+
+  @override
+  State<SliderDemo> createState() => _SliderDemoState();
+}
+
+class _SliderDemoState extends State<SliderDemo> {
+  late double _currentSliderValue = 0.0;
+  late Position _currentPosition;
+
+  void initState() {
+    super.initState();
+    _initLocation();
+  }
+
+  void _initLocation() async {
+    _currentPosition = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      _currentSliderValue = _currentPosition.latitude;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('Selected radius: ${_currentSliderValue.toStringAsFixed(6)} km'),
+          Slider(
+            value: _currentSliderValue,
+            min: -90,
+            max: 90,
+            divisions: 180,
+            label: _currentSliderValue.toStringAsFixed(6),
+            onChanged: (double value) {
+              setState(() {
+                _currentSliderValue = value;
+                _currentPosition = Position(
+                    longitude: _currentSliderValue,
+                    latitude: _currentPosition.longitude,
+                    timestamp: _currentPosition.timestamp,
+                    accuracy: _currentPosition.accuracy,
+                    altitude: _currentPosition.altitude,
+                    heading: _currentPosition.heading,
+                    speed: _currentPosition.speed,
+                    speedAccuracy: _currentPosition.speedAccuracy);
+              });
+            },
+          ),
+        ],
       ),
     );
   }
