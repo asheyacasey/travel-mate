2d75e20ab9dcd2d015d68c3068b4fc21b483e10c
Vonne Harvey <vonne.tuso@usjr.edu.ph>
2023-06-28 23:23:05 +0800

    business address search autocomplete

--- lib/business/screens/business_info.dart
+++ lib/business/screens/business_info.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:travel_mate/business/screens/business_home.dart';
 
 class BusinessInfoScreen extends StatefulWidget {
@@ -15,6 +18,7 @@ class _BusinessInfoScreenState extends State<BusinessInfoScreen> {
   final TextEditingController _businessNameController = TextEditingController();
   final TextEditingController _businessAddressController =
       TextEditingController();
+  List<String> _addressSuggestions = [];
   String? _businessNameError;
   String? _businessAddressError;
   bool _isFormValid = false;
@@ -39,24 +43,44 @@ class _BusinessInfoScreenState extends State<BusinessInfoScreen> {
 
     if (_isBusinessNameEmpty(businessName) &&
         _isBusinessAddressEmpty(businessAddress)) {
-      DocumentReference businessDocRef =
-          FirebaseFirestore.instance.collection('business').doc(widget.userId);
+      final url = Uri.parse(
+          'https://nominatim.openstreetmap.org/search?format=json&q=$businessAddress');
+      final response = await http.get(url);
 
-      businessDocRef.set({
-        'businessName': businessName,
-        'businessAddress': businessAddress,
-      }).then((value) {
-        print('Business information stored successfully!');
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => BusinessHomeScreen()),
-        );
-      }).catchError((error) {
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body) as List<dynamic>;
+        if (data.isNotEmpty) {
+          final lat = double.parse(data[0]['lat']);
+          final lon = double.parse(data[0]['lon']);
+
+          DocumentReference businessDocRef = FirebaseFirestore.instance
+              .collection('business')
+              .doc(widget.userId);
+
+          businessDocRef.update({
+            'businessName': businessName,
+            'businessAddress': businessAddress,
+            'latitude': lat,
+            'longitude': lon,
+          }).then((value) {
+            print('Business information stored successfully!');
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => BusinessHomeScreen()),
+            );
+          }).catchError((error) {
+            setState(() {
+              _isFormValid = false;
+            });
+            print('Failed to store business information: $error');
+          });
+        }
+      } else {
         setState(() {
           _isFormValid = false;
         });
-        print('Failed to store business information: $error');
-      });
+        print('Failed to fetch coordinates for the address');
+      }
     }
   }
 
@@ -78,6 +102,38 @@ class _BusinessInfoScreenState extends State<BusinessInfoScreen> {
       return false;
     }
     return true;
+  }
+
+  Future<List<String>> _getAddressSuggestions(String query) async {
+    try {
+      final url = Uri.parse(
+          'https://nominatim.openstreetmap.org/search?format=json&q=$query');
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body) as List<dynamic>;
+        final suggestions =
+            data.map((item) => item['display_name'] as String).toList();
+        return suggestions;
+      } else {
+        throw Exception(
+            'Failed to load address suggestions. Status Code: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Failed to load address suggestions: $e');
+    }
+  }
+
+  void _updateAddressSuggestions(String query) {
+    _getAddressSuggestions(query).then((suggestions) {
+      // Show the suggestions to the user (e.g., using a ListView)
+      print(suggestions);
+      setState(() {
+        _addressSuggestions = suggestions;
+      });
+    }).catchError((error) {
+      print('Failed to load address suggestions: $error');
+    });
   }
 
   @override
@@ -114,12 +170,39 @@ class _BusinessInfoScreenState extends State<BusinessInfoScreen> {
               'Business Address',
               style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             ),
-            TextFormField(
-              controller: _businessAddressController,
-              decoration: InputDecoration(
-                hintText: 'Enter your business address',
-                errorText: _businessAddressError,
-              ),
+            Column(
+              children: [
+                TextFormField(
+                  controller: _businessAddressController,
+                  decoration: InputDecoration(
+                    hintText: 'Enter your business address',
+                    errorText: _businessAddressError,
+                  ),
+                  onChanged: (query) {
+                    _updateAddressSuggestions(query);
+                  },
+                ),
+                SizedBox(height: 10),
+                if (_addressSuggestions.isNotEmpty)
+                  Container(
+                    height: 200,
+                    child: ListView.builder(
+                      itemCount: _addressSuggestions.length,
+                      itemBuilder: (context, index) {
+                        final suggestion = _addressSuggestions[index];
+                        return ListTile(
+                          title: Text(suggestion),
+                          onTap: () {
+                            setState(() {
+                              _businessAddressController.text = suggestion;
+                              _addressSuggestions = [];
+                            });
+                          },
+                        );
+                      },
+                    ),
+                  ),
+              ],
             ),
             SizedBox(height: 20),
             ElevatedButton(
