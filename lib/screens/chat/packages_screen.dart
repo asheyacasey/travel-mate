bb9d1287093010e14de1776ef347d74517d6e5bd
Vonne Harvey <vonne.tuso@usjr.edu.ph>
2023-07-05 21:23:26 +0800

    imported packageDetails to packages screen

--- lib/screens/chat/packages_screen.dart
+++ lib/screens/chat/packages_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:travel_mate/screens/chat/packageDetails_screen.dart';
 
 class PackagesScreen extends StatelessWidget {
   final int numberOfDays;
