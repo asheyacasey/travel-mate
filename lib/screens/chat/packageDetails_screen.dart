193c590121b9fb64a3c7f23f56bf2a32e2f2e49f
Vonne Harvey <vonne.tuso@usjr.edu.ph>
2023-07-05 20:45:38 +0800

    fixed some importing of classes

--- lib/screens/chat/packageDetails_screen.dart
+++ lib/screens/chat/packageDetails_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travel_mate/screens/chat/packages_screen.dart';
 
 class PackageDetailsScreen extends StatelessWidget {
   final Package package;
@@ -32,36 +33,4 @@ TimeOfDay addDurationToTime(TimeOfDay time, int duration) {
   newMinute = newMinute % 60;
 
   return TimeOfDay(hour: newHour, minute: newMinute);
-}
-
-class Package {
-  final List<Activity> activities;
-
-  Package({this.activities = const []});
-
-  int get totalDuration {
-    int total = 0;
-    activities.forEach((activity) {
-      total += activity.duration;
-    });
-    return total;
-  }
-}
-
-class Activity {
-  final String activityName;
-  final String category;
-  final String address;
-  final TimeOfDay timeStart;
-  final TimeOfDay timeEnd;
-  final int duration;
-
-  Activity({
-    required this.activityName,
-    required this.category,
-    required this.address,
-    required this.timeStart,
-    required this.timeEnd,
-    required this.duration,
-  });
 }
