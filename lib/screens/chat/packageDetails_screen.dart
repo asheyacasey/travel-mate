5d8b4b8fa3c2ad9f011854229e2b96a2e8141d69
Vonne Harvey <vonne.tuso@usjr.edu.ph>
2023-07-07 21:09:10 +0800

    created dayWidgets to separate activities by day

--- lib/screens/chat/packageDetails_screen.dart
+++ lib/screens/chat/packageDetails_screen.dart
@@ -8,21 +8,47 @@ class PackageDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> dayWidgets = [];
+    List<Activity> activities = package.activities;
+
+    int totalDuration = 0;
+    int currentDay = 1;
+
+    for (int i = 0; i < activities.length; i++) {
+      Activity activity = activities[i];
+      totalDuration += activity.duration;
+
+      if (totalDuration > 600) {
+        currentDay++;
+        totalDuration = activity.duration;
+      }
+
+      String dayTitle = 'Day $currentDay';
+
+      if (!dayWidgets.contains(dayTitle)) {
+        dayWidgets.add(
+          Text(
+            dayTitle,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        );
+      }
+
+      dayWidgets.add(
+        ListTile(
+          title: Text(activity.activityName),
+          subtitle: Text(
+            'Category: ${activity.category}\nAddress: ${activity.address}\nTime: ${activity.timeStart.format(context)} - ${addDurationToTime(activity.timeStart, activity.duration).format(context)}',
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Package Details'),
       ),
-      body: ListView.builder(
-        itemCount: package.activities.length,
-        itemBuilder: (context, index) {
-          Activity activity = package.activities[index];
-          return ListTile(
-            title: Text(activity.activityName),
-            subtitle: Text(
-                'Category: ${activity.category}\nAddress: ${activity.address}\nTime: ${activity.timeStart.format(context)} - ${addDurationToTime(activity.timeStart, activity.duration).format(context)}'),
-          );
-        },
-      ),
+      body: ListView(children: dayWidgets),
     );
   }
 }
